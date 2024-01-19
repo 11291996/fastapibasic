6b6a7dbc01f09978c5310f096490e0069c3485b3
jaewanpark <jaewan1040@yonsei.ac.kr>
2024-01-19 20:29:43 +0900

    01192024

--- fastapibasic.py
+++ fastapibasic.py
@@ -105,6 +105,55 @@ async def get_model(model_name: ModelName): #type detection is also possible
 async def read_file(file_path: str): 
     return {"file_path": file_path}
 
+#query parameter -> non path parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+#http://127.0.0.1:8000/items/?skip=0&limit=10 -> use this convention to pass multiple parameters
+#if one of them is or both are not defined, it will use the default value
+
+#using python annotations
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+#type conversion 
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#if default value is not defined, it will be required
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy} #if needy is not defined, it will return an error
+    return item
+
 #request body
 #one can send json, form data, files, etc. to the server through request body
 #to declare a request body, use pydantic models
