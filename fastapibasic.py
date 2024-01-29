7300edcbdf28f80eae127e9d1fb3d9b39c79b917
11291996 <jaewan1040@yonsei.ac.kr>
2024-01-29 17:09:28 +0900

    01292024

--- fastapibasic.py
+++ fastapibasic.py
@@ -860,18 +860,15 @@ from fastapi.exception_handlers import (
     request_validation_exception_handler,
 )
 
-
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request, exc):
     print(f"OMG! An HTTP error!: {repr(exc)}")
     return await http_exception_handler(request, exc)
 
-
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     print(f"OMG! The client sent invalid data!: {exc}")
     return await request_validation_exception_handler(request, exc)
-
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
