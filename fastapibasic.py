1e1b149d9a0e2bcf49fb7357b7885a17e67f8b52
11291996 <jaewan1040@yonsei.ac.kr>
2024-03-21 17:31:46 +0900

    03212024

--- fastapibasic.py
+++ fastapibasic.py
@@ -999,6 +999,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 #the python code will be like this "response = requests.get(URL, headers={"Authorization":"Bearer token"})"
 #similar to https' request header
 #then the url will send back a token as the reponse that authenticates the user and expires after a certain time
+#this way will be safer for other user interferences
+#does the same role as django's escape function
 #frontend will store the token and utilize it for requests that need authentications
 @app.get("/items/")
 #this dependency will check that the input of the function is from OAuth2 module
@@ -1081,3 +1083,143 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     return {"access_token": user.username, "token_type": "bearer"}
+
+#security level hashing
+#uses Json Web Tokens(JWT) to ready the token for encryption
+#install python-jose and passlib
+
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+#for token expiration time
+from datetime import datetime, timedelta, timezone
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+#algorithm for encryption
+ALGORITHM = "HS256"
+#token expire time variable
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+#if these are hacked, it can be very dangerous
+
+oauth2_scheme_real = OAuth2PasswordBearer(tokenUrl="real_token")
+
+#class to store the token data
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+#class to store token corresponding data
+class TokenData(BaseModel):
+    username: str | None = None
+
+#passlib instance for encryption
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+fake_users_db_real = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        #this is jwt and passlib encrypted password of "secret"
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+#checking the hashed password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+#hashing the password
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+#comparing from the database
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+#creates a token with expiration time
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    #adding expiration time to the token
+    to_encode.update({"exp": expire})
+    #using jwt to encrypt the token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+#checking the user from the database
+async def get_current_user_real(token: Annotated[str, Depends(oauth2_scheme_real)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        #using jwt to decode the token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        #storing the token corresponding data
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    #checking and returning the user from the database
+    user = get_user(fake_users_db_real, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+#for raising current user error
+async def get_current_active_user_real(
+    current_user: Annotated[User, Depends(get_current_user_real)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+#real working login path
+@app.post("/real_token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = authenticate_user(fake_users_db_real, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    #creates expiration time for the token using set variable
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    #creates the token
+    access_token = create_access_token(
+        #jwt notation for the subject of the token
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+#uses jwt and passlib to get the current user
+@app.get("/users/current_me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user_real)]
+):
+    return current_user
+
+#uses jwt and passlib to get the current user's items
+@app.get("/users/current_me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user_real)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+#similarly one can store the user data in the database and use it for the login
